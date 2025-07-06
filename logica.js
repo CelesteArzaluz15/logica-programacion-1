154224dfb169a545ef4641b3723efbe95bd22c9d
celeste <celeste.arzaluz15@gmail.com>
2025-07-06 00:38:42 -0600

    Logica de programacion 1

--- logica.js
+++ logica.js
@@ -26,15 +26,34 @@ function procesarNumeros(nums) {
     return;
   }
 
-  if (nums[0] === nums[1] && nums[1] === nums[2]) {
-    console.log("Los tres números son iguales:", nums);
-  } else {
-    let mayorAMenor = [...nums].sort((a, b) => b - a);
-    let menorAMayor = [...nums].sort((a, b) => a - b);
+  const [a, b, c] = nums;
 
-    console.log("De mayor a menor:", mayorAMenor.join(", "));
-    console.log("De menor a mayor:", menorAMayor.join(", "));
+  // Contador de ocurrencias
+  const conteo = {};
+
+  nums.forEach(num => {
+    conteo[num] = (conteo[num] || 0) + 1;
+  });
+
+  const valoresRepetidos = Object.entries(conteo).filter(([num, count]) => count > 1);
+
+  // Mensaje según repeticiones
+  if (valoresRepetidos.length === 1 && valoresRepetidos[0][1] === 3) {
+    console.log(`Los tres números son iguales: ${valoresRepetidos[0][0]}`);
+  } else if (valoresRepetidos.length >= 1) {
+    valoresRepetidos.forEach(([num, count]) => {
+      console.log(`El número ${num} se repite ${count} veces.`);
+    });
+  } else {
+    console.log("Todos los números son diferentes.");
   }
+
+  // Ordenamientos
+  const mayorAMenor = [...nums].sort((x, y) => y - x);
+  const menorAMayor = [...nums].sort((x, y) => x - y);
+
+  console.log("De mayor a menor:", mayorAMenor.join(", "));
+  console.log("De menor a mayor:", menorAMayor.join(", "));
 }
 
 pedirNumero(0);
